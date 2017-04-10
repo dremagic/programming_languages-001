034e93bfc940a790f38f19c36b57f410cb5abfe1
dremagic <deandre.ellison@gmail.com>
2017-04-10 15:40:57 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+languages.each do |style,language_data|
+  language_data.each do |language,language_info|    
+    new_hash.merge!(language => language_info) if new_hash[language].nil?
+   # binding.pry
+
+    new_hash[language][:style] = [] if new_hash[language][:style].nil? 
+   # binding.pry
+    new_hash[language][:style] << style
+  
+  end
+end
+# binding.pry
+
+
+new_hash
 end
